0509d018e8fe8b80687bfc3e08c85c84015cfe53
Artem Kolesnikov <artemiyk2017@gmail.com>
2026-04-16 13:24:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 def copy_file(command: str) -> None:
     command_words = command.split()
-    print(command_words)
-    try:
+    if len(command_words) == 3:
         if command_words[0].lower() == "cp":
-            file_source = command_words[1].lower()
-            file_dest = command_words[2].lower()
-            with (
-                open(file_source, "r") as file_source,
-                open(file_dest, "w") as file_dest
-            ):
-                file_dest.write(file_source.read())
-    except IndexError as e:
-        print(f"{e}: Not enough arguments")
-    except FileNotFoundError as e:
-        print(f"{e}: File not found")
+            file_source = command_words[1]
+            file_dest = command_words[2]
+            if file_source != file_dest:
+                try:
+                    with (
+                        open(file_source, "r") as file_source,
+                        open(file_dest, "w") as file_dest
+                    ):
+                        file_dest.write(file_source.read())
+                except FileNotFoundError:
+                    print("File not found")
